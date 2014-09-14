78d4f9f650bfa6692b43b8698f7aeed7d22bca8e
Cora Allen-Coleman <coraallencoleman@gmail.com>
2014-09-14 16:15:14 -0400

    Work on a binary to decimal converter - not finished

---
+++ BinarytoDecimalConverter.py
@@ -0,0 +1,18 @@
+#Binary to Decimal and Back Converter - Develop a converter to convert a decimal
+#number to binary or a binary number to its decimal equivalent.
+
+def decimal_to_binary(number):
+    number_list =[int(i) for i in str(number)]
+    print number_list
+    
+
+#base = raw_input("Would you like to convert a number from decimal to binary? (Y/N)")
+
+#if base = "Y":
+#    number = raw_input("Enter a number to be converted from decimal to binary: ")
+#    print decimal_to_binary(number)
+#    
+#else:
+#    number = raw_input("Enter a number to be converted from binary to decimal: ")
+#    print binary_to_decimal(binary)
+    
